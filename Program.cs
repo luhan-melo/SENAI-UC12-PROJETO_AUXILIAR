f1601d533af42145339bb6b5b8636b305f8e8a9e
Luhan Melo <luhanalves@outlook.com>
2022-08-20 19:04:25 -0300

    atualização do codigo conforme encontro remoto 7, sistema para cadastrar conta corrente e conta poupança parte 1

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ Console.WriteLine(@$"
 *************************************************************************************
 ");
 
+List<Corrente> listaCC = new List<Corrente>();
+List<Poupanca> listaCP = new List<Poupanca>();
+
+BarraCarregamento("Carregando", 200);
+Console.Clear();
+
+
 Console.BackgroundColor = ConsoleColor.DarkGray;
 
 Console.Write("Carregando ");
@@ -29,6 +36,7 @@ string? opcao;
 
 do
 {
+    Console.Clear();
     Console.WriteLine(@$"
 *************************************************************************************
 *************************************************************************************
@@ -36,7 +44,7 @@ do
 *                       Escolha uma das opções a seguir:                            *
 *                                                                                   *
 *                               1 - Conta Corrente                                  *
-*                               2 - Conta Corrente                                  *
+*                               2 - Conta Poupança                                  *
 *                               0 - Sair                                            *
 *                                                                                   *
 *                                                                                   *
@@ -49,22 +57,201 @@ do
     switch (opcao)
     {
         case "1":
-            Corrente obj_cc = new Corrente("3030-0", "1988", 101);
-            Console.WriteLine($"{obj_cc.ToString()}");
-            Console.WriteLine($"Aperte 'Enter' para continuar");
-            Console.ReadLine();
-            Console.Clear();
+            string? opcaoCC;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@$"
+*************************************************************************************
+*************************************************************************************
+*                                                                                   *
+*                       Escolha uma das opções a seguir:                            *
+*                                                                                   *
+*                               1 - Cadastrar Conta Corrente                        *
+*                               2 - Mostrar Contas Correntes                        *
+*                               0 - Voltar ao Menu Anterior                         *
+*                                                                                   *
+*                                                                                   *
+*************************************************************************************
+*************************************************************************************
+");
+
+                opcaoCC = Console.ReadLine();
+
+                switch (opcaoCC)
+                {
+                    case "1":
+                        Console.WriteLine($"Digite o número da conta corrente");
+                        string numero = Console.ReadLine();
+
+                        Console.WriteLine($"Digite o número da agencia");
+                        string agencia = Console.ReadLine();
+
+
+                        Console.WriteLine($"Digite o número do limite");
+                        double limite = Double.Parse(Console.ReadLine());
+
+
+
+                        Corrente obj_cc = new Corrente(numero, agencia, limite);
+
+                        listaCC.Add(obj_cc);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Cadastro Realizado com Sucesso!!!");
+                        Console.ResetColor();
+
+                        Console.WriteLine($"Aperte 'Enter' para continuar");
+                        Console.ReadLine();
+                        // Console.Clear();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        if (listaCC.Count > 0)
+                        {
+                            foreach (Corrente cadaCC in listaCC)
+                            {
+                                Console.Clear();
+                                Console.WriteLine($"{cadaCC.ToString()}");
+                                Console.WriteLine($"Aperte 'Enter' para continuar");
+                                Console.ReadLine();
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"A lista está vazia!!!");
+                            Thread.Sleep(3000);
+                        }
+
+                        break;
+
+
+                    case "0":
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine($"Opção inválida, por favor digite outra opção");
+                        Thread.Sleep(3000);
+                        break;
+                }
+
+
+            } while (opcaoCC != "0");
+
+            /*
+                Corrente obj_cc = new Corrente("3030-0", "1988", 101);
+                Console.WriteLine($"{obj_cc.ToString()}");
+                Console.WriteLine($"Aperte 'Enter' para continuar");
+                Console.ReadLine();
+                Console.Clear();
+                break;
+                */
             break;
 
         case "2":
-            Corrente obj_cpp = new Corrente("1010-0", "1978", 3);
-            Console.WriteLine($"{obj_cpp.ToString()}");
-            Console.WriteLine($"Aperte 'Enter' para continuar");
-            Console.ReadLine();
-            Console.Clear();
+            //conta poupança
+
+            string? opcaoCP;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@$"
+*************************************************************************************
+*************************************************************************************
+*                                                                                   *
+*                       Escolha uma das opções a seguir:                            *
+*                                                                                   *
+*                               1 - Cadastrar Conta Poupança                        *
+*                               2 - Mostrar Contas Poupanças                        *
+*                               0 - Voltar ao Menu Anterior                         *
+*                                                                                   *
+*                                                                                   *
+*************************************************************************************
+*************************************************************************************
+");
+
+                opcaoCP = Console.ReadLine();
+
+                switch (opcaoCP)
+                {
+                    case "1":
+                        Console.WriteLine($"Digite o número da conta poupança:");
+                        string numero = Console.ReadLine();
+
+                        Console.WriteLine($"Digite o número da agencia: ");
+                        string agencia = Console.ReadLine();
+
+
+                        Console.WriteLine($"Digite o aniversário: ");
+                        int aniversario = Int32.Parse(Console.ReadLine());
+
+
+
+                        Poupanca obj_cp = new Poupanca(numero, agencia, aniversario);
+
+                        listaCP.Add(obj_cp);
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Cadastro Realizado com Sucesso!!!");
+                        Console.ResetColor();
+
+                        Console.WriteLine($"Aperte 'Enter' para continuar");
+                        Console.ReadLine();
+                        // Console.Clear();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        if (listaCP.Count > 0)
+                        {
+                            foreach (Poupanca cadaCP in listaCP)
+                            {
+                                Console.Clear();
+                                Console.WriteLine($"{cadaCP.ToString()}");
+                                Console.WriteLine($"Aperte 'Enter' para continuar");
+                                Console.ReadLine();
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"A lista está vazia!!!");
+                            Thread.Sleep(3000);
+                        }
+
+                        break;
+
+
+                    case "0":
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine($"Opção inválida, por favor digite outra opção");
+                        Thread.Sleep(3000);
+                        break;
+                }
+
+
+            } while (opcaoCP != "0");
             break;
 
+
+        /*  
+                    Corrente obj_cpp = new Corrente("1010-0", "1978", 3);
+                    Console.WriteLine($"{obj_cpp.ToString()}");
+                    Console.WriteLine($"Aperte 'Enter' para continuar");
+                    Console.ReadLine();
+                    Console.Clear();
+        */
+
+
+
         case "0":
+            Console.Clear();
+            Console.WriteLine($"Obrigado por utilizar nosso sistema");
+            BarraCarregamento("Finalizando", 200);
             Console.WriteLine($"Voce entrou na opção 0");
             break;
 
@@ -81,6 +268,20 @@ do
 
 } while (opcao != "0");
 
+
+static void BarraCarregamento(string texto, int tempo)
+{
+    Console.BackgroundColor = ConsoleColor.DarkCyan;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write($"{texto}");
+
+    for (var contador = 0; contador < 10; contador++)
+    {
+        Console.Write(". ");
+        Thread.Sleep(tempo);
+    }
+    Console.ResetColor();
+}
 
 
 /*
